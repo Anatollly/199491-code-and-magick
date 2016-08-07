9a6a7daead81ce74226ded1ca372d0aff44db3b9
Anatoly Kovalev <anatolko@yandex.ru>
2016-08-07 18:50:33 +0700

    Исправлены ошибки

--- build/js/check.js
+++ build/js/check.js
@@ -1,15 +1,15 @@
 function getMessage(a, b) {
-  var numberOfSteps = 0, distancePath = 0;
+ var numberOfSteps = 0, distancePath = 0;
 
-  if( typeof(a) == "boolean" ){
-    return a===true ? ('Я попал в ' + b) : "Я никуда не попал";
+  if( typeof(a) === "boolean" ){
+    return a === true ? ('Я попал в ' + b) : "Я никуда не попал";
   }
 
   if(typeof(a) == "number"){
     return 'Я прыгнул на ' + a*100 + ' сантиметров';
   }
 
-  if(typeof(a) == "object" && typeof(b) == "object"){
+  if(Array.isArray(a) === true && Array.isArray(b) === true){
 
     if(a.length <= b.length){
     d = a.length;
@@ -19,15 +19,15 @@ function getMessage(a, b) {
     }
 
     for (i = 0; i<d; i++){
-      distancePath = distancePath + a[i]*b[i];
+      distancePath += a[i]*b[i];
     }
     return 'Я прошёл ' + distancePath + ' метров';
   }
 
-  if(typeof(a) == "object"){
+  if(Array.isArray(a) === true){
 
     for (var i = 0; i<a.length; i++) {
-      numberOfSteps = numberOfSteps + a[i];
+      numberOfSteps += a[i];
     }
     return 'Я прошёл ' + numberOfSteps + ' шагов';
   }
