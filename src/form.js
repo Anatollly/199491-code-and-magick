04565c33c2b6c4c7d87e366d6583aa5286e07a1d
Anatoly Kovalev <anatolko@yandex.ru>
2016-08-19 17:05:16 +0700

    Добавлена валидация формы отправки отзыва

--- src/form.js
+++ src/form.js
@@ -4,6 +4,64 @@ window.form = (function() {
   var formContainer = document.querySelector('.overlay-container');
   var formCloseButton = document.querySelector('.review-form-close');
 
+  var reviewForm = document.querySelector('.review-form');
+  var rating = reviewForm.elements['review-mark'];
+  var name = reviewForm.elements['review-name'];
+  var text = reviewForm.elements['review-text'];
+
+  var button = document.querySelector('.review-submit');
+  var fieldLinks = document.querySelector('.review-fields');
+  var linkName = document.querySelector('.review-fields-name');
+  var linkText = document.querySelector('.review-fields-text');
+
+  var LOW_RATING = 3;
+
+  name.required = true;
+
+  function validateForm() {
+    text.required = rating.value < LOW_RATING;
+
+    var validName = name.value;
+    var validText = !text.required || text.value;
+
+    if (validName) {
+      linkName.classList.add('invisible');
+    }else {
+      linkName.classList.remove('invisible');
+    }
+
+    if (validText) {
+      linkText.classList.add('invisible');
+    }else {
+      linkText.classList.remove('invisible');
+    }
+
+    if (validText && validName) {
+      fieldLinks.classList.add('invisible');
+    }else {
+      fieldLinks.classList.remove('invisible');
+    }
+
+    button.disabled = !(validText && validName);
+  }
+
+  validateForm();
+
+  name.oninput = function() {
+    validateForm();
+  };
+  //nameField.addEventListener('input', validateForm);
+  text.oninput = function() {
+    validateForm();
+  };
+  //feedbackField.addEventListener('input', validateForm);
+
+  for (var i = 0; i < rating.length; i++) {
+    rating[i].onclick = function() {
+      validateForm();
+    };
+  }
+
   var form = {
     onClose: null,
 
