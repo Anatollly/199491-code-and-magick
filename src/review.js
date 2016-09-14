c4bbafd409155101936e9d764b5a5608aa50fe97
Anatoly Kovalev <anatolko@yandex.ru>
2016-09-14 19:37:49 +0700

    Добавлен конструктор Review

--- src/review.js
+++ src/review.js
@@ -13,8 +13,6 @@ define(function() {
 
   var getReviewElement = function(review) {
     var reviewElement = templateContainer.querySelector('.review').cloneNode(true);
-    var reviewsContainer = document.querySelector('.reviews-list');
-    reviewsContainer.appendChild(reviewElement);
     reviewElement.querySelector('.review-rating').style.width = (ICON_STAR_WIDTH * review.rating) + 'px';
     reviewElement.querySelector('.review-text').textContent = review.description;
     var reviewImage = reviewElement.querySelector('.review-author');
@@ -41,6 +39,35 @@ define(function() {
     return reviewElement;
   };
 
-  return getReviewElement;
+  var Review = function(data) {
+    this.data = data;
+    this.element = getReviewElement(data);
+    this.answerYes = this.element.querySelector('.review-quiz-answer-yes');
+    this.answerNo = this.element.querySelector('.review-quiz-answer-no');
+    var self = this;
+    this.answerYes.onclick = function() {
+      self.clickYes();
+    };
+    this.answerNo.onclick = function() {
+      self.clickNo();
+    };
+  };
+
+  Review.prototype.clickYes = function() {
+    this.answerYes.classList.add('review-quiz-answer-active');
+    this.answerNo.classList.remove('review-quiz-answer-active');
+  };
+
+  Review.prototype.clickNo = function() {
+    this.answerYes.classList.remove('review-quiz-answer-active');
+    this.answerNo.classList.add('review-quiz-answer-active');
+  };
+
+  Review.prototype.remove = function() {
+    this.answerYes.onclick = null;
+    this.answerNo.onclick = null;
+  };
+
+  return Review;
 
 });
--- src/reviews.js
+++ src/reviews.js
@@ -1,23 +1,21 @@
 'use strict';
 
-define(['./load', './review'], function(load, getReview) {
+define(['./load', './review'], function(load, Review) {
 
-
-  var reviews = null;
 
   var elem = document.querySelector('.reviews-filter');
+  var reviewsContainer = document.querySelector('.reviews-list');
 
-  var addReviews = function(data) {
-    reviews = data;
+  var addReviews = function(reviews) {
     reviews.forEach(function(review) {
-      getReview(review);
+      var rev = new Review(review);
+      reviewsContainer.appendChild(rev.element);
     });
-    elem.classList.add('invisible');
   };
 
-  window.onload = function() {
-    elem.classList.remove('invisible');
-  };
+
+  elem.classList.remove('invisible');
+
 
   load('http://localhost:1506/api/reviews', 'jsonCallback', addReviews);
 
