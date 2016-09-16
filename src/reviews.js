0940ab3ad0c57a5d3a62967f837b7fed5f55a6ed
Anatoly Kovalev <anatolko@yandex.ru>
2016-09-16 12:03:57 +0700

    Добавлен постраничный вывод отзывов и их фильтрация

--- src/reviews.js
+++ src/reviews.js
@@ -2,9 +2,14 @@
 
 define(['./load', './review'], function(load, Review) {
 
+  var PAGE_SIZE = 3;
+  var pageNumber = 0;
+  var activeFilter = 'reviews-all';
 
-  var elem = document.querySelector('.reviews-filter');
+  var reviewsFilter = document.querySelector('.reviews-filter');
   var reviewsContainer = document.querySelector('.reviews-list');
+  var reviewsMore = document.querySelector('.reviews-controls-more');
+  reviewsMore.classList.remove('invisible');
 
   var addReviews = function(reviews) {
     reviews.forEach(function(review) {
@@ -13,10 +18,29 @@ define(['./load', './review'], function(load, Review) {
     });
   };
 
+  var loadReviews = function(currentPageNumber, filter) {
+    load('http://localhost:1506/api/reviews', {
+      from: currentPageNumber * PAGE_SIZE,
+      to: PAGE_SIZE + currentPageNumber * PAGE_SIZE,
+      filter: filter
+    }, addReviews);
+  };
 
-  elem.classList.remove('invisible');
+  loadReviews(pageNumber, activeFilter);
 
+  var changeFilters = function(filterID) {
+    reviewsContainer.innerHTML = '';
+    activeFilter = filterID;
+    pageNumber = 0;
+    loadReviews(pageNumber, filterID);
+  };
 
-  load('http://localhost:1506/api/reviews', 'jsonCallback', addReviews);
+  reviewsMore.addEventListener('click', function() {
+    pageNumber++;
+    loadReviews(pageNumber, activeFilter);
+  });
 
+  reviewsFilter.addEventListener('change', function(evt) {
+    changeFilters(evt.target.id);
+  });
 });
